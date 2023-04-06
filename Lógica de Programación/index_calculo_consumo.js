d89f2cfa25b3021de4fb315e54f60131d3f1fcaf
Hastur5 <mgasca12@gmail.com>
2023-04-06 16:36:48 -0600

    feat: se agrega problema de la gasolina y el alcohol

---
+++ Lógica de Programación/index_calculo_consumo.js
@@ -0,0 +1,7 @@
+let tanque = 40
+let recorridoGasolina = 480
+let recorridoAlcohol = 300
+let kmGasolina = recorridoGasolina / tanque
+let kmAlcohol = recorridoAlcohol / tanque
+
+document.write('El auto recorre: ' + kmGasolina + ' kilómetros por litro con gasolina. ' + 'Con alcohol, el auto recorre ' + kmAlcohol + ' por litro.'  )
---
+++ Lógica de Programación/index_segundo_test.js
@@ -0,0 +1,55 @@
+// let numero1 = 50
+// let numero2 = 30
+
+// let nombre = 'Fili'
+// let apellido = 'Negro'
+
+// document.write('<h1>Esto es Rojo</h1>')
+// document.write(numero1 + numero2)
+// document.write('<br>')
+// document.write(nombre + ' ' + apellido)
+// document.write('<br>')
+// document.write((20+25+15)/3)
+// document.write('<br>')
+// document.write(20+25+15)
+
+var año = 2025
+
+var edadPaola = 22
+var edadJimena = 11
+var edadLaura = 28
+
+var nombre = "Jimena"
+var nombre1 = "Paola"
+var nombre2 = "Laura"
+
+var promedio = Math.round((edadJimena + edadLaura + edadPaola)/3)
+
+document.write('Juan tiene: ' + (año-2000) + ' años')
+document.write('<br>')
+document.write('<br>')
+
+año = 2050
+
+document.write('Pedro tiene: ' + (año-1995) + ' años')
+document.write('<br>')
+document.write('<br>')
+document.write('Carlos tiene: ' + (año-2005) + ' años')
+
+año = 2020
+document.write('<br>')
+document.write('<br>')
+document.write('Jimena tiene: ' + (año-2009) + ' años')
+
+document.write('<br>')
+document.write('<br>')
+document.write('Paola tiene: ' + (año-1998) + ' años')
+
+document.write('<br>')
+document.write('<br>')
+document.write('Laura tiene: ' + (año-1992) + ' años')
+
+document.write('<br>')
+document.write('<br>')
+document.write('El promedio de las edades de: ' + nombre + ', ' + nombre1 + ' y ' + nombre2  + ' es: ' + promedio)
+
