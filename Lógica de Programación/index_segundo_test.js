868f1fab4a05010946b33f715e75ae4808ef1e4b
Hastur5 <mgasca12@gmail.com>
2023-04-11 02:50:02 -0600

    feat: se agrega la parte de funciones

--- Lógica de Programación/index_segundo_test.js
+++ Lógica de Programación/index_segundo_test.js
@@ -14,42 +14,41 @@
 // document.write(20+25+15)
 
 var año = 2025
-
 var edadPaola = 22
 var edadJimena = 11
 var edadLaura = 28
-
 var nombre = "Jimena"
 var nombre1 = "Paola"
 var nombre2 = "Laura"
-
 var promedio = Math.round((edadJimena + edadLaura + edadPaola)/3)
-
-document.write('Juan tiene: ' + (año-2000) + ' años')
-document.write('<br>')
-document.write('<br>')
+let saltoLinea = '<br> <br> <br>'
 
 año = 2050
 
-document.write('Pedro tiene: ' + (año-1995) + ' años')
-document.write('<br>')
-document.write('<br>')
-document.write('Carlos tiene: ' + (año-2005) + ' años')
-
 año = 2020
-document.write('<br>')
-document.write('<br>')
-document.write('Jimena tiene: ' + (año-2009) + ' años')
 
-document.write('<br>')
-document.write('<br>')
-document.write('Paola tiene: ' + (año-1998) + ' años')
+function saltoDeLinea () {
+    document.write('<br>')
+    document.write('<br>')
+    document.write('<br>')
+}
 
-document.write('<br>')
-document.write('<br>')
-document.write('Laura tiene: ' + (año-1992) + ' años')
+function imprimir (frase) {
+    document.write(frase)
+    saltoDeLinea()
+}
 
-document.write('<br>')
-document.write('<br>')
-document.write('El promedio de las edades de: ' + nombre + ', ' + nombre1 + ' y ' + nombre2  + ' es: ' + promedio)
+imprimir('Juan tiene: ' + (año-2000) + ' años')
+//document.write(saltoLinea)
+imprimir('Pedro tiene: ' + (año-1995) + ' años')
+//document.write(saltoLinea)
+imprimir('Carlos tiene: ' + (año-2005) + ' años')
+//document.write(saltoLinea)
+imprimir('Jimena tiene: ' + (año-2009) + ' años')
+//document.write(saltoLinea)
+imprimir('Paola tiene: ' + (año-1998) + ' años')
+//document.write(saltoLinea)
+imprimir('Laura tiene: ' + (año-1992) + ' años')
+//document.write(saltoLinea)
+imprimir('El promedio de las edades de: ' + nombre + ', ' + nombre1 + ' y ' + nombre2  + ' es: ' + promedio)
 
