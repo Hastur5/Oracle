b600276ca467609ea6daa172e292bed9d6e2e793
Hastur5 <mgasca12@gmail.com>
2023-04-14 20:27:43 -0600

    feat: subo programa de adivinación de números con 3 instentos máximo.

--- Primeros Pasos/Ejercicio/Adivinación/index.js
+++ Primeros Pasos/Ejercicio/Adivinación/index.js
@@ -1,17 +1,25 @@
 let numeroPensado = Math.round(Math.random() * 10);
-let numeroLanzado = parseInt(prompt("Elige un número entre 0 y 10"));
+let i = 0;
+let limite = 3;
 
-function saltoDeLinea() {
-  document.write("<br><br><br>");
-}
+console.log(numeroPensado);
 
 function imprimir(frase) {
   document.write(frase);
-  saltoDeLinea();
+}
+
+while (i < limite) {
+  numeroLanzado = parseInt(prompt("Elige un número entre 0 y 10"));
+  console.log(numeroLanzado);
+  if (numeroPensado == numeroLanzado) {
+    break;
+  } else {
+    i = i + 1;
+  }
 }
 
 if (numeroPensado == numeroLanzado) {
-  imprimir("Usted acertó");
+  imprimir("Usted acertó, el número pensado fue: " + numeroPensado);
 } else {
   imprimir("Usted erró, el número pensado era: " + numeroPensado);
 }
