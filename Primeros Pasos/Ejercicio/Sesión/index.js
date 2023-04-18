a15dc196f546b9253ce793afd876423609ffa322
Hastur5 <mgasca12@gmail.com>
2023-04-17 21:16:19 -0600

    se agrega inicio de sesión

--- Primeros Pasos/Ejercicio/Sesión/index.js
+++ Primeros Pasos/Ejercicio/Sesión/index.js
@@ -1,11 +1,18 @@
-    var inicioDeSesionRegistrado = "alura";
-    var contrasenhaRegistrada = "alura321";
+var inicioDeSesionRegistrado = "alura";
+var contrasenhaRegistrada = "alura321";
 
-    var inicioDeSesionIngresado = prompt("Ingrese su usuario");
-    var contrasenhaIngresada = prompt("Ingrese su contraseña");
-
-    if( inicioDeSesionRegistrado == inicioDeSesionIngresado && contrasenhaRegistrada == contrasenhaIngresada ) {
-        alert("Bienvenido al sistema " + inicioDeSesionIngresado);
-    } else {
-        alert("inicio de sesión inválido. Favor intente de nuevo");
+for (let i = 1; i <= 3; i++) {
+  let contrasenhaIngresada = prompt("Ingrese su contraseña");
+  let inicioDeSesionIngresado = prompt("Ingrese su usuario");
+  if (
+    inicioDeSesionRegistrado == inicioDeSesionIngresado &&
+    contrasenhaRegistrada == contrasenhaIngresada
+  ) {
+    alert("Bienvenido al sistema " + inicioDeSesionIngresado);
+  } else {
+    alert("inicio de sesión inválido. Favor intente de nuevo");
+    if ((i == 3)) {
+      alert("Ha excedido el número de intentos");
     }
+  }
+}
